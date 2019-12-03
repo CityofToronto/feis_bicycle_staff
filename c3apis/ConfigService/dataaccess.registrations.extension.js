d64a3bb87843ce0f226ec78f3b943f8ed6dcb26f
James Ngo <James.Ngo@toronto.ca>
2019-12-03 11:54:12 -0500

    Clean Up

--- c3apis/ConfigService/dataaccess.registrations.extension.js
+++
@@ -1,9 +0,0 @@
-// function afterQuery(content, request, uriInfo, response) { }
-
-// function beforeContentParse(content, request, uriInfo, response) { }
-
-// function afterCreate(content, request, uriInfo, response) { }
-
-// function afterUpdate(content, request, uriInfo, response) { }
-
-// function afterDelete(content, request, uriInfo, response) { }
