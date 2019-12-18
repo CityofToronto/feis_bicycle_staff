ed7d152433d7ab408167b178281b71055dff7b31
James Ngo <James.Ngo@toronto.ca>
2019-12-18 15:42:52 -0500

    Add Back Main Script and Style

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+// The main javascript file for bicycle_staff_2.
+// IMPORTANT:
+// Any resources from this project should be referenced using SRC_PATH preprocessor var
+// Ex: let myImage = '/*@echo SRC_PATH*//img/sample.jpg';
+
+/* global $ */
+/* global cot_app */
+
+$(function () {
+  const app = new cot_app('Bicycle Parking', {
+    hasContentTop: false,
+    hasContentBottom: false,
+    hasContentRight: false,
+    hasContentLeft: false,
+    searchcontext: 'INTRA'
+  });
+  app.setBreadcrumb([]).render();
+});
