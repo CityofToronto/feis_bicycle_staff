4beadcf483bb4536adcff4cd8b86de019f0ae017
James Ngo <James.Ngo@toronto.ca>
2019-12-23 04:56:33 -0500

    More File Name Changes

--- c3apis/ConfigService/dataaccess.location_notes.extension.js
+++ c3apis/ConfigService/dataaccess.location_notes.extension.js
@@ -1,16 +1,19 @@
+'use strict';
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // REQUIRE
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const common = require('bicycle_parking/common.js');
+var common = require('bicycle_parking/common.js');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* exported afterQuery, beforeContentParse, afterCreate, afterUpdate, afterDelete */
 
-function afterQuery(content, request, uriInfo, response) { // eslint-disable-line no-unused-vars
+function afterQuery(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
 
-function beforeContentParse(content, request, uriInfo, response) { // eslint-disable-line no-unused-vars
+function beforeContentParse(content, request, uriInfo, response) {
+  // eslint-disable-line no-unused-vars
   if (request.getMethod() === 'POST') {
     if (content.has('__Status')) {
       content.remove('__Status');
@@ -20,11 +23,11 @@ function beforeContentParse(content, request, uriInfo, response) { // eslint-dis
   }
 }
 
-function afterCreate(content, request, uriInfo, response) { // eslint-disable-line no-unused-vars
+function afterCreate(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
 
-function afterUpdate(content, request, uriInfo, response) { // eslint-disable-line no-unused-vars
+function afterUpdate(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
 
-function afterDelete(content, request, uriInfo, response) { // eslint-disable-line no-unused-vars
+function afterDelete(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
---
+++ src/scripts/configurations/location_notes.js
@@ -0,0 +1,89 @@
+/* global moment */
+
+/* exported location_notes_datatable_columns */
+const location_notes_datatable_columns = {
+  action: {
+    title: 'Action',
+    className: 'excludeFromButtons openButtonWidth',
+    data: 'id',
+    orderable: false,
+    searchable: false
+  },
+
+  location: {
+    title: 'Locker Location',
+    className: 'minWidth',
+    data: 'location'
+  },
+  date: {
+    title: 'Date',
+    className: 'minWidth',
+    data: 'date',
+    type: 'date',
+    render(data) {
+      const dataMoment = moment(data);
+      if (dataMoment.isValid()) {
+        return dataMoment.format('YYYY/MM/DD');
+      } else {
+        return '';
+      }
+    }
+  },
+  note: {
+    title: 'Note',
+    data: 'note',
+    render(data) {
+      if (data) {
+        return data.replace(/(?:\r\n|\r|\n)/g, '<br>');
+      } else {
+        return '';
+      }
+    }
+  },
+
+  __CreatedOn: {
+    title: 'Created On',
+    className: 'minWidth',
+    data: '__CreatedOn',
+    type: 'date',
+    render(data) {
+      const dataMoment = moment(data);
+      if (dataMoment.isValid()) {
+        return dataMoment.format('YYYY/MM/DD h:mm:ss A');
+      } else {
+        return '';
+      }
+    }
+  },
+  __ModifiedOn: {
+    title: 'Modified On',
+    className: 'minWidth',
+    data: '__ModifiedOn',
+    type: 'date',
+    render(data) {
+      const dataMoment = moment(data);
+      if (dataMoment.isValid()) {
+        return dataMoment.format('YYYY/MM/DD h:mm:ss A');
+      } else {
+        return '';
+      }
+    }
+  },
+  __Owner: {
+    title: 'Modified By',
+    className: 'minWidth',
+    data: '__Owner',
+    type: 'string'
+  },
+  __Status: {
+    title: 'Status',
+    className: 'statusWidth',
+    data: '__Status',
+    type: 'string',
+    searchType: 'equals',
+    choices: [{ text: 'Active' }, { text: 'Inactive' }],
+    render(data) {
+      return `<span class="label label-${data === 'Active' ? 'success' : data === 'Inactive' ? 'danger' : 'default'}" style="font-size: 90%;">${data}</span>`;
+    }
+  }
+};
