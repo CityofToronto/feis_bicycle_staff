968f080c8eaf85f988611614d30f591418529255
James Ngo <James.Ngo@toronto.ca>
2019-12-23 19:26:52 -0500

    Prepare Location Inspection

---
+++ src/scripts/configurations/location_inspections.js
@@ -0,0 +1,205 @@
+/* global $ moment */
+
+/* exported location_inspections_datatable_columns */
+const location_inspections_datatable_columns = () => ({
+  action: {
+    title: 'Action',
+    className: 'excludeFromButtons openButtonWidth',
+    data: 'id',
+    orderable: false,
+    searchable: false
+  },
+
+  location: {
+    title: 'Locker Location',
+    className: 'minWidth',
+    data: 'location'
+  },
+  date: {
+    title: 'Date',
+    className: 'minWidth',
+    data: 'date',
+    type: 'date',
+    render(data) {
+      const dataMoment = moment(data);
+      if (dataMoment.isValid()) {
+        return dataMoment.format('YYYY/MM/DD');
+      } else {
+        return '';
+      }
+    }
+  },
+  note: {
+    title: 'Note',
+    className: 'minWidthLarge',
+    data: 'note',
+    render(data) {
+      if (data) {
+        return data.replace(/(?:\r\n|\r|\n)/g, '<br>');
+      } else {
+        return '';
+      }
+    }
+  },
+
+  location__site_name: {
+    title: 'Locker Location',
+    className: 'minWidth',
+    data: 'location__site_name'
+  },
+
+  __CreatedOn: {
+    title: 'Created On',
+    className: 'minWidth',
+    data: '__CreatedOn',
+    type: 'date',
+    render(data) {
+      const dataMoment = moment(data);
+      if (dataMoment.isValid()) {
+        return dataMoment.format('YYYY/MM/DD h:mm A');
+      } else {
+        return '';
+      }
+    }
+  },
+  __ModifiedOn: {
+    title: 'Modified On',
+    className: 'minWidth',
+    data: '__ModifiedOn',
+    type: 'date',
+    render(data) {
+      const dataMoment = moment(data);
+      if (dataMoment.isValid()) {
+        return dataMoment.format('YYYY/MM/DD h:mm A');
+      } else {
+        return '';
+      }
+    }
+  },
+  __Owner: {
+    title: 'Modified By',
+    className: 'minWidth',
+    data: '__Owner',
+    type: 'string'
+  },
+  __Status: {
+    title: 'Status',
+    className: 'statusWidth',
+    data: '__Status',
+    type: 'string',
+    searchType: 'equals',
+    choices: [{ text: 'Active' }, { text: 'Inactive' }],
+    render(data) {
+      return `<span class="label label-${data === 'Active' ? 'success' : data === 'Inactive' ? 'danger' : 'default'}" style="font-size: 90%;">${data}</span>`;
+    }
+  }
+});
+
+/* exported location_inspection_form_sections */
+const location_inspection_form_sections = (auth) => [
+  {
+    title: 'Details',
+
+    rows: [
+      {
+        fields: [
+          {
+            title: 'Locker Location',
+            bindTo: 'location',
+            required: true,
+            className: 'col-sm-8',
+            type: 'dropdown',
+            choices: {
+              beforeSend(jqXHR) {
+                if (auth && auth.sId) {
+                  jqXHR.setRequestHeader('Authorization', `AuthSession ${auth.sId}`);
+                }
+              },
+              contentType: 'application/json; charset=utf-8',
+              method: 'GET',
+              url: '/* @echo C3DATA_LOCATIONS_URL */?$select=id,site_name&$filter=__Status eq \'Active\''
+            },
+            choicesMap(data) {
+              if (data && data.value) {
+                return data.value.sort((a, b) => {
+                  const a_site_name = a.site_name.toLowerCase();
+                  const b_site_name = b.site_name.toLowerCase();
+                  if (a_site_name > b_site_name) {
+                    return 1;
+                  }
+                  if (a_site_name < b_site_name) {
+                    return -1;
+                  }
+                  return 0;
+                }).map((item) => {
+                  return {
+                    text: item.site_name,
+                    value: item.id
+                  }
+                });
+              }
+              return [];
+            },
+            postRender({ model, field }) {
+              function displayHandler() {
+                if (model.isNew()) {
+                  $(`#${field.id}Element`).removeClass('hide');
+                } else {
+                  $(`#${field.id}Element`).addClass('hide');
+                }
+              }
+              displayHandler();
+              model.on(`change:${model.idAttribute}`, displayHandler);
+            }
+          },
+          {
+            title: 'Locker Location',
+            bindTo: 'location__site_name',
+            required: true,
+            className: 'col-sm-8',
+            type: 'text',
+            htmlAttr: { readonly: true },
+            postRender({ model, field }) {
+              function valueHandler() {
+                $(`#${field.id}`).val(model.get(field.bindTo));
+              }
+              valueHandler();
+              model.on(`change:${field.bindTo}`, valueHandler);
+
+              function displayHandler() {
+                if (model.isNew()) {
+                  $(`#${field.id}Element`).addClass('hide');
+                } else {
+                  $(`#${field.id}Element`).removeClass('hide');
+                }
+              }
+              displayHandler();
+              model.on(`change:${model.idAttribute}`, displayHandler);
+            }
+          },
+          {
+            title: 'Date',
+            bindTo: 'date',
+            required: true,
+            className: 'col-sm-4',
+            type: 'datetimepicker',
+            options: {
+              format: 'YYYY/MM/DD'
+            }
+          }
+        ]
+      },
+      {
+        fields: [
+          {
+            title: 'Note',
+            bindTo: 'note',
+            required: true,
+            type: 'textarea',
+            rows: 10
+          }
+        ]
+      }
+    ]
+  }
+];
---
+++ src/scripts/pages/locationinspectiondetailspage.js
@@ -0,0 +1,144 @@
+/* global $ Backbone moment */
+/* global ajaxes auth__checkLogin fixButtonLinks modal__showConfirm query__objectToString query__stringToObject
+   renderAlert toSnapShot */
+/* global renderForm */
+/* global location_note_form_sections */
+
+/* exported renderLocationInspectionDetailsPage */
+function renderLocationInspectionDetailsPage(app, $container, router, auth, opt, id, query) {
+  return auth__checkLogin(auth).then((isLoggedIn) => {
+    if (!isLoggedIn) {
+      const query = query__objectToString({ redirect: Backbone.history.getFragment() });
+      router.navigate(`login?${query}`, { trigger: true });
+      return;
+    }
+
+    const breadcrumbs = [{ name: app.name, link: '#home' }, { name: 'Location Notes', link: '#location_notes' }];
+    switch (opt) {
+      default:
+        breadcrumbs.push({ name: 'All', link: `#location_notes/all` });
+    }
+
+    const {
+      redirectTo = 'Location Notes',
+      redirectToFragment = `location_notes/${opt}`
+    } = query__stringToObject(query);
+    $container.html(`<p><a href="#${redirectToFragment}">Back to ${redirectTo}</a></p>`);
+
+    return Promise.resolve().then(() => {
+      if (id !== 'new') {
+        return ajaxes({
+          beforeSend(jqXHR) {
+            if (auth && auth.sId) {
+              jqXHR.setRequestHeader('Authorization', `AuthSession ${auth.sId}`);
+            }
+          },
+          contentType: 'application/json; charset=utf-8',
+          method: 'GET',
+          url: `/* @echo C3DATA_LOCATION_NOTES_URL */('${id}')`
+        });
+      }
+
+      return { data: {} };
+    }).then(({ data }) => {
+      const $tabContainer = $('<div></div>').appendTo($container);
+      function renderNavBar(finalId) {
+        $tabContainer.html(`
+          <div class="navbar">
+            <ul class="nav nav-tabs">
+              <li class="nav-item active" role="presentation">
+                <a href="#location_notes/${opt}/${finalId}" class="nav-link">Location Note</a>
+              </li>
+            </ul>
+          </div>
+        `);
+        fixButtonLinks($tabContainer);
+      }
+      if (id !== 'new') {
+        renderNavBar(id);
+      }
+
+      const definition = {
+        successCore(data, options = {}) {
+          const { auth, id, url } = options;
+
+          return ajaxes({
+            url: `${url}${id ? `('${id}')` : ''}`,
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            method: id ? 'PUT' : 'POST',
+            beforeSend(jqXHR) {
+              if (auth && auth.sId) {
+                jqXHR.setRequestHeader('Authorization', `AuthSession ${auth.sId}`);
+              }
+            }
+          }).then(({ data, textStatus, jqXHR }) => {
+            snapShot = toSnapShot(data);
+            renderNavBar(data.id);
+
+            router.navigate(`location_notes/${opt}/${data.id}`, { trigger: false, replace: true });
+            app.setBreadcrumb(breadcrumbs.concat({ name: data.site_name }), true);
+            app.setTitle(data.site_name);
+
+            return { data, textStatus, jqXHR };
+          });
+        },
+
+        sections: location_note_form_sections(auth)
+      };
+
+      const Model = Backbone.Model.extend({
+        defaults: {
+          date: moment().format('YYYY/MM/DD')
+        }
+      });
+      const model = new Model(data);
+      let snapShot = toSnapShot(model.toJSON());
+
+      return Promise.resolve().then(() => {
+        return renderForm($('<div></div>').appendTo($container), definition, model, {
+          auth,
+          url: '/* @echo C3DATA_LOCATION_NOTES_URL */',
+
+          saveButtonLabel: (model) => model.isNew() ? 'Create Location Note' : 'Update Location Note',
+
+          cancelButtonLabel: 'Cancel',
+          cancelButtonFragment: `location_notes/${opt}`,
+
+          removeButtonLabel: 'Remove Location Note',
+          removePromptValue: 'DELETE'
+        });
+      }).then(() => {
+        if (id === 'new') {
+          app.setBreadcrumb(breadcrumbs.concat({ name: 'New Location Note' }), true);
+          app.setTitle('New Location Note');
+        } else {
+          app.setBreadcrumb(breadcrumbs.concat({ name: data.date }), true);
+          app.setTitle(data.site_name);
+        }
+
+        return () => {
+          if (snapShot !== toSnapShot(model.toJSON())) {
+            return modal__showConfirm('There may be one or more unsaved data. Do you want to continue?', {
+              title: 'Confirm',
+              confirmButtonLabel: 'Continue'
+            });
+          }
+        };
+      });
+    }, (error) => {
+      app.setBreadcrumb(breadcrumbs.concat({ name: 'Error' }), true);
+      app.setTitle('An Error has Occured');
+
+      renderAlert($container, '<p>An error occured while fetching data.</p>', {
+        bootstrayType: 'danger',
+        position: 'bottom'
+      })
+
+      console.error(error); // eslint-disable-line no-console
+    });
+  }, (error) => {
+    console.error(error); // eslint-disable-line no-console
+  });
+}
