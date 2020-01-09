1cdcfb7fbd0795c9c46bce4e0ddb08d27860647d
James Ngo <James.Ngo@toronto.ca>
2020-01-08 21:01:14 -0500

    Start Customer and Payment

--- c3apis/ConfigService/dataaccess.payment_notifications.extension.js
+++ c3apis/ConfigService/dataaccess.payment_notifications.extension.js
@@ -1,21 +1,15 @@
-'use strict';
+"use strict";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // REQUIRE
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var common = require('bicycle_parking/common.js');
+// const common = require('bicycle_parking/common.js');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* exported afterQuery, beforeContentParse, afterCreate, afterUpdate, afterDelete */
 
-function afterQuery(content, request, uriInfo, response) {
-  // eslint-disable-line no-unused-vars
-  if (common.SSJS_DISABLED) {
-    return;
-  }
-
-  setStatus(content, request);
+function afterQuery(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
 
 function beforeContentParse(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
@@ -27,19 +21,5 @@ function afterCreate(content, request, uriInfo, response) {// eslint-disable-lin
 function afterUpdate(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
 
-function afterDelete(content, request, uriInfo, response) {} // eslint-disable-line no-unused-vars
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function setStatus(content, request) {
-  if (request.getMethod() !== 'POST') {
-    return;
-  }
-
-  if (content.has('__Status')) {
-    content.remove('__Status');
-  }
-
-  content.addProperty('__Status', 'Active');
+function afterDelete(content, request, uriInfo, response) {// eslint-disable-line no-unused-vars
 }
